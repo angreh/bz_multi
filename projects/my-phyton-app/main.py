e180fd2230756ab34190760bd203ebc2124d4018
Angreh Pinto <angreh@gmail.com>
2024-05-31 19:58:01 +0200

    teste

--- projects/my-phyton-app/main.py
+++ projects/my-phyton-app/main.py
@@ -10,7 +10,7 @@ my_calculator = Calculator()
 def hello():
   num1 = randint(0, 100)
   num2 = randint(0, 100)
-  message = "Did you know {} + {} = {}?".format(num1, num2, my_calculator.add(num1, num2))
+  message = "Did you know? {} + {} = {}?".format(num1, num2, my_calculator.add(num1, num2))
   return message
 
 if __name__ == '__main__':
